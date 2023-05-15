ba5aa0c727d99d0c951841d023ea312f54f87659
tami055 <ktami3116@gmail.com>
2023-05-16 01:48:59 +0300

    Your commit message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import {createRequire} from 'module'
-const require = createRequire(import.meta.url);
+import {require} from 'module'
+const require = require(import.meta.url);
 const app=express()
 const port=3000
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
