18af296ce2627ba30c680a6348c124a3ea0e8b5d
tami055 <ktami3116@gmail.com>
2023-05-16 00:58:28 +0300

    Your commit message here

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-console.log('hi');
-
+import express from 'express'
+import {createRequire} from 'module'
+const require = createRequire(import.meta.url);
+const app=express()
+const port=3000
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 sdk.auth('rnd_bVtXtBNqRyVa5AQupmi5UGtXi5eD');
 sdk.getServices({limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
+  app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+app.listen(port,()=>{
+  console.log(`app listening on http://localhost:${port}`)
+})
